23acec009b981fc36cfcf52fe8e6d2de4e4f499d
ichwalM <13020230049@student.umi.ac.id>
2025-06-24 09:12:46 +0800

    input output

---
+++ day_5/test1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    int umur;
+    cout << "umur : "; cin>>umur;cin.ignore();
+    cout << "name : "; getline(cin, name);
+
+}
