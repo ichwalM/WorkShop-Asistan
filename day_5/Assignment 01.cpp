0b527f301e74983237757655bebddc99f4710e19
ichwalM <13020230049@student.umi.ac.id>
2025-06-24 13:08:43 +0800

    fiishing

--- day_5/Assignment 01.cpp
+++ day_5/Assignment 01.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void tampil(int ary[],int lenght){
-    cout<<"Hasil \t: ";
+    cout<<"Hasil \t\t: ";
     for(int i=0;i<lenght;i++)cout<<ary[i]<< " ";
 }
 
@@ -22,6 +22,12 @@ int terbesar(int ary[],int lenght){
     return tmp;
 }
 
+float rataRata(int ary[], int lenght){
+    float tmp=0;
+    for(int i=0;i<lenght;i++)tmp+=ary[i];
+    return tmp/lenght;
+}
+
 int diAtasRata(int ary[],int lenght,float rata){
     int tmp = 0;
     for(int i=0;i<lenght;i++){
@@ -35,17 +41,12 @@ int main(){
     int ary[lenght];
     cout<<"Input Nilai \t: ";
     for(int i=0;i<lenght;i++)cin>>ary[i];
-
-
-    float rata,tmp=0;
-    for(int i=0;i<lenght;i++){
-        tmp+=ary[i];
-    }
-    rata=tmp/lenght;
+    float rata;
+    rata=(float)rataRata(ary,lenght);
     tampil(ary,lenght);
     cout<<endl;
-    cout<<"Nilai Terbesar : "<< terbesar(ary,lenght)<<endl;
-    cout<<"Nilai Terkecil : "<< terkecil(ary,lenght)<<endl;
+    cout<<"Nilai Terbesar \t: "<< terbesar(ary,lenght)<<endl;
+    cout<<"Nilai Terkecil \t: "<< terkecil(ary,lenght)<<endl;
     cout<<"Rata-rata \t: "<<rata<<endl;
     cout<<"Bilangan Di atas Rata-rata : "<< diAtasRata(ary,lenght,rata);
 
